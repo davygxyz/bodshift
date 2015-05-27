6e3a5a45a948542f410ff70dcfb1a4cc67fe3ca3
davygxyz <davygxyz@gmail.com>
2015-05-27 00:54:41 -0400

    finished donations page mock up

--- resources/views/pages/donate.blade.php
+++ resources/views/pages/donate.blade.php
@@ -8,15 +8,26 @@
 <div class='row'>
 	<div class='col-sm-12 col-lg-6'>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et ligula iaculis, convallis nulla vitae, aliquet nisi. Donec lacinia volutpat sem et suscipit. Maecenas pharetra eros quis porttitor accumsan. Donec porttitor elementum risus, ac egestas est. Ut feugiat dui urna, et hendrerit purus cursus sit amet. Duis mauris sapien, imperdiet nec diam ut, placerat facilisis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum justo sed ligula blandit vehicula et ac nulla. In nulla odio, imperdiet sit amet ante in, suscipit cursus augue. Proin pulvinar pulvinar diam et sodales. Donec lacinia a sapien porttitor lobortis. Aenean sagittis sodales dolor vel tristique. Sed aliquam purus eu tincidunt blandit. Phasellus dignissim nisi sed ullamcorper posuere. Mauris nunc dui, commodo sit amet lobortis ut, laoreet ut ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-
-Pellentesque euismod ultricies diam sit amet facilisis. Nam accumsan venenatis ante, quis auctor enim. Integer viverra enim vitae accumsan auctor. Suspendisse potenti. Cras sollicitudin quis lacus sit amet ultrices. Duis elementum elit quis tincidunt euismod. Sed ut ligula dictum, porta sem laoreet, rhoncus odio. Integer vestibulum enim et pretium pulvinar. Aliquam eget dui molestie, auctor orci sed, accumsan tellus. Sed placerat lectus eget nisi lobortis, ac consequat ligula dapibus. Praesent fermentum sodales dolor, et tincidunt leo. Nam sed viverra augue, at semper neque. Vestibulum tincidunt nulla mattis, venenatis tortor in, condimentum turpis. Aliquam congue porta vestibulum. Nam ut orci quis mi sagittis auctor. Duis sit amet dignissim erat, nec aliquet tellus.
-
-Sed vulputate sem at condimentum bibendum. Duis non dapibus velit. Maecenas non velit nec augue interdum efficitur a vitae orci. Pellentesque euismod porttitor tortor in scelerisque. Fusce massa nunc, maximus in urna et, vehicula blandit ante. Praesent est libero, tempor nec interdum eget, accumsan id metus. Curabitur varius quam sit amet mauris tempus, ut elementum purus luctus.
+			Pellentcitur a vitae orci. Pellentesque euismod porttitor tortor i in urna et, vehicula blandit ante. Praesent est libero, tempor nec interdum eget, accumsan id metus. Curabitur varius quam sit amet mauris tempus, ut elementum purus luctus.
 		</p>
 	</div>
 	<div class='col-sm-12 col-lg-6'>
-		<img src="http://placehold.it/600x300" alt='ad-space-right' class="img-responsive">
+		<div class='row bottom-margin'>
+			<div class='col-sm-12'>
+				<img src="http://placehold.it/600x100" alt='ad-space-right' class="img-responsive">
+			</div>
+		</div>
+		<div class='row'>
+			<div class='col-sm-12'>
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style='text-align:center;'>
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="K7HDHKKBTXC5N">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+			</div>
+		</div>
+
 	</div>
 </div>
 @stop
